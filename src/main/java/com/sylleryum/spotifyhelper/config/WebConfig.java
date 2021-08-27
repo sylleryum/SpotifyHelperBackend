fbc82e08afb167139fd26ce8e850e51cc6a48e1c
sylleryum <alisson_piuco@yahoo.com.br>
2021-08-27 20:07:54 -0300

    add cors

--- src/main/java/com/sylleryum/spotifyhelper/config/WebConfig.java
+++ src/main/java/com/sylleryum/spotifyhelper/config/WebConfig.java
@@ -12,7 +12,7 @@ public class WebConfig extends WebMvcConfigurerAdapter {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-//                .allowedOrigins("http://localhost:4200")
+                .allowedOrigins("https://sylleryum.github.io/","http://localhost:4200/")
                 .allowCredentials(true)
                 .allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
     }
